7cfe056f33322a615046b59cbc436bb2b3a7c32d
Alperen <alperenoz1464@gmail.com>
2021-02-07 00:04:01 +0300

    listing blogs in admin/blogs page

--- MyTravelBlogs/Controllers/AdminController.cs
+++ MyTravelBlogs/Controllers/AdminController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyTravelBlogs.Models;
 
 namespace MyTravelBlogs.Controllers
 {
     public class AdminController : Controller
     {
-        
+        private readonly MyContext _myContext;
+        public AdminController(MyContext myContext)
+        {
+            _myContext = myContext;
+        }
+
         public IActionResult Blogs()
         {
-            return View();
+            var blogs = _myContext.blogs.ToList();
+            return View(blogs);
         }
 
         public IActionResult Update()
